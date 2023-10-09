45dfaf3959145b434c7a4761e32955522fe3f25e
2sky <2skydev@gmail.com>
2023-10-09 21:34:10 +0900

    ✏️ typo: metadata title 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import ThemeProvider from '@/features/theme/ThemeProvider'
 import '@/styles/index.css'
 
 export const metadata: Metadata = {
-  title: '로스트아크 레이드 매니저',
+  title: 'RAID STUDIO',
   description:
     '레이드 고정 멤버와 진행한 레이드를 기록하고 공유, 일정 관리를 할 수 있는 서비스입니다.',
 }
