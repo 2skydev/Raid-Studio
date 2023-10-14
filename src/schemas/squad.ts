49910292363afb6e9ff11cf1beb6a01914219083
2sky <2skydev@gmail.com>
2023-10-14 19:59:32 +0900

    ✨ feat: 공격대, 유저 이름 정규식 추가

--- src/schemas/squad.ts
+++ src/schemas/squad.ts
@@ -5,6 +5,7 @@ import { usersCollectionSchema } from '@/schemas/user'
 export const squadsCollectionSchema = z.object({
   name: z
     .string({ invalid_type_error: '공격대명 형식이 올바르지 않습니다.' })
+    .regex(/^[a-zA-Z0-9가-힣]+$/, { message: '공격대명은 영문, 숫자, 한글만 입력 가능합니다.' })
     .min(1, { message: '공격대명은 최소 1자 이상으로 입력해주세요' })
     .max(12, { message: '공격대명은 최대 12자 이하로 입력해주세요' }),
   userIds: z.array(usersCollectionSchema.shape.id),
