cf9a54d2f3f4188f4ecc05a8255e18b235260028
2sky <2skydev@gmail.com>
2023-10-18 20:50:59 +0900

    ✨ feat: 선택한 공격대 localStorage 저장

--- src/stores/selectedSquadIdAtom.ts
+++ src/stores/selectedSquadIdAtom.ts
@@ -1,8 +1,20 @@
-import { atomWithDefault } from 'jotai/utils'
+import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
 import { mySquadsAtom } from '@/stores/mySquadsAtom'
 
-export const selectedSquadIdAtom = atomWithDefault(async get => {
-  const mySquads = await get(mySquadsAtom)
-  return mySquads[0]?.id
-})
+const KEY = 'jotai.selectedSquadId'
+
+const persistenceAtom = atomWithStorage<null | string>(KEY, null)
+
+export const selectedSquadIdAtom = atom(
+  async get => {
+    const selectedSquadId = get(persistenceAtom)
+
+    const mySquads = await get(mySquadsAtom)
+    return selectedSquadId || mySquads[0]?.id
+  },
+  (get, set, id: string) => {
+    set(persistenceAtom, id)
+  },
+)
