e69a84c3e40c1a7b2673b831ad8b4a672f34faee
2sky <2skydev@gmail.com>
2023-10-18 20:50:09 +0900

    ✨ feat: SquadMemberManageDataTable 로딩 효과 추가

--- src/app/studio/squad/members/page.tsx
+++ src/app/studio/squad/members/page.tsx
@@ -12,25 +12,35 @@ import { selectedSquadIdAtom } from '@/stores/selectedSquadIdAtom'
 
 const StudioSquadMembersPage = () => {
   const selectedSquadId = useAtomValue(selectedSquadIdAtom)
-  const { data } = useAPI<SquadMembers>(`/squads/${selectedSquadId}/members`, {
-    disabled: !selectedSquadId,
-  })
+  const { data: members, isLoading: isLoadingFromSquadMembers } = useAPI<SquadMembers>(
+    `/squads/${selectedSquadId}/members`,
+    {
+      disabled: !selectedSquadId,
+    },
+  )
 
-  const { data: code } = useAPI<{ code: string }>(`/squads/${selectedSquadId}/code`, {
-    disabled: !selectedSquadId,
-  })
+  const { data: code, isLoading: isLoadingFromSquadCode } = useAPI<{ code: string }>(
+    `/squads/${selectedSquadId}/code`,
+    {
+      disabled: !selectedSquadId,
+    },
+  )
+
+  const isLoading = isLoadingFromSquadMembers || isLoadingFromSquadCode
 
   return (
     <div>
       <h1 className={css({ fontSize: '2xl', fontWeight: 'bold' })}>공격대 멤버 관리</h1>
-
       <p className={css({ color: 'muted.foreground' })}>
         내 프로필, 대표 캐릭터, 팀 등을 설정할 수 있습니다
       </p>
-
       <br />
 
-      {data && code && <SquadMemberManageDataTable data={data} code={code.code} />}
+      <SquadMemberManageDataTable
+        data={members || []}
+        code={code?.code || ''}
+        isLoading={isLoading}
+      />
     </div>
   )
 }
--- src/features/squad/SquadMemberManageDataTable/SquadMemberManageDataTable.tsx
+++ src/features/squad/SquadMemberManageDataTable/SquadMemberManageDataTable.tsx
@@ -7,6 +7,7 @@ import {
   getFilteredRowModel,
   useReactTable,
 } from '@tanstack/react-table'
+import { Share2Icon } from 'lucide-react'
 
 import { css } from '@styled-system/css'
 import { Box, Flex } from '@styled-system/jsx'
@@ -17,6 +18,7 @@ import CopyButton from '@/components/CopyButton'
 import { DataTableColumnHeader } from '@/components/DataTable/ColumnHeader'
 import { Input } from '@/components/Input'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/Popover'
+import Skeleton from '@/components/Skeleton'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/Table'
 
 import { SquadMembers } from '@/schemas/squad'
@@ -24,6 +26,7 @@ import { SquadMembers } from '@/schemas/squad'
 export interface SquadMemberManageDataTableProps {
   data: SquadMembers
   code: string
+  isLoading?: boolean
 }
 
 const columns: ColumnDef<SquadMembers[number]>[] = [
@@ -51,7 +54,7 @@ const columns: ColumnDef<SquadMembers[number]>[] = [
   },
 ]
 
-const SquadMemberManageDataTable = ({ data, code }: SquadMemberManageDataTableProps) => {
+const SquadMemberManageDataTable = ({ data, code, isLoading }: SquadMemberManageDataTableProps) => {
   const table = useReactTable({
     data,
     columns,
@@ -61,80 +64,101 @@ const SquadMemberManageDataTable = ({ data, code }: SquadMemberManageDataTablePr
   return (
     <div className={css({ maxW: '3xl' })}>
       <Flex align="center" justifyContent="space-between" py="4">
-        <Input
-          placeholder="닉네임으로 검색..."
-          value={(table.getColumn('profile')?.getFilterValue() as string) ?? ''}
-          onChange={event => table.getColumn('profile')?.setFilterValue(event.target.value)}
-          maxW="sm"
-        />
+        {isLoading ? (
+          <Skeleton w="sm" h="10" />
+        ) : (
+          <Input
+            placeholder="닉네임으로 검색..."
+            value={(table.getColumn('profile')?.getFilterValue() as string) ?? ''}
+            onChange={event => table.getColumn('profile')?.setFilterValue(event.target.value)}
+            w="sm"
+          />
+        )}
 
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button>초대하기</Button>
-          </PopoverTrigger>
+        {isLoading ? (
+          <Skeleton w="32" h="10" />
+        ) : (
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button w="32">
+                <Share2Icon size="1rem" />
+                초대하기
+              </Button>
+            </PopoverTrigger>
 
-          <PopoverContent w="80">
-            <h2 className={css({ fontSize: 'md', fontWeight: 'medium' })}>참가 코드로 초대하기</h2>
-            <p className={css({ fontSize: 'sm', color: 'muted.foreground' })}>
-              참가 코드로 공격대 목록에서 참가 할 수 있습니다.
-            </p>
-            <div
-              className={css({
-                mt: '2',
-                p: '2',
-                pl: '3',
-                border: 'base',
-                rounded: 'md',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              })}
-            >
-              <span className={css({ fontSize: 'sm' })}>{code}</span>
-              <CopyButton value={code} />
-            </div>
-          </PopoverContent>
-        </Popover>
+            <PopoverContent w="80">
+              <h2 className={css({ fontSize: 'md', fontWeight: 'medium' })}>
+                참가 코드로 초대하기
+              </h2>
+              <p className={css({ fontSize: 'sm', color: 'muted.foreground' })}>
+                참가 코드로 공격대 목록에서 참가 할 수 있습니다.
+              </p>
+              <div
+                className={css({
+                  mt: '2',
+                  p: '2',
+                  pl: '3',
+                  border: 'base',
+                  rounded: 'md',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                })}
+              >
+                <span className={css({ fontSize: 'sm' })}>{code}</span>
+                <CopyButton value={code} />
+              </div>
+            </PopoverContent>
+          </Popover>
+        )}
       </Flex>
 
       <Box w="full" rounded="md" border="base">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map(headerGroup => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map(header => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableHead>
-                  )
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map(row => (
-                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                  {row.getVisibleCells().map(cell => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
+        {isLoading ? (
+          <Box p="2" spaceY="2">
+            <Skeleton w="full" h="12" />
+            <Skeleton w="full" h="12" />
+            <Skeleton w="full" h="12" />
+          </Box>
+        ) : (
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map(headerGroup => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map(header => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(header.column.columnDef.header, header.getContext())}
+                      </TableHead>
+                    )
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} h="24" textAlign="center">
-                  데이터가 없습니다.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHeader>
+
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map(row => (
+                  <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                    {row.getVisibleCells().map(cell => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={columns.length} h="24" textAlign="center">
+                    데이터가 없습니다.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        )}
       </Box>
     </div>
   )
