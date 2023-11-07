9a284019a880de65484cadcdfeab48f09838164c
2sky <2skydev@gmail.com>
2023-11-07 17:56:05 +0900

    ✨ feat: supabase 초기 세팅

---
+++ src/types/database.types.ts
@@ -0,0 +1,10 @@
+import { Database as DatabaseGenerated } from './database-generated.types'
+
+export type { Json } from './database-generated.types'
+
+export type Database = DatabaseGenerated
+
+export type Tables<T extends keyof Database['public']['Tables']> =
+  Database['public']['Tables'][T]['Row']
+
+export type Enums<T extends keyof Database['public']['Enums']> = Database['public']['Enums'][T]
